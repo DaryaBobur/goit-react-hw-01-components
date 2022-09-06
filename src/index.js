7031a96fb8a1e804465ba22ccb7e1a32cc630cd7
Darya <daryabobur@gmail.com>
2022-09-06 21:55:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,51 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+import user from './user.json';
+console.log(user);
+
+function Profile ({username, tag, location, avatar, stats}) {
+  return (
+<div className="profile">
+  <div className="description">
+    <img
+      src={avatar}
+      alt="User avatar"
+      className="avatar"
+    />
+    <p className="name">{username}</p>
+    <p className="tag">@{tag}</p>
+    <p className="location">{location}</p>
+  </div>
+
+  <ul className="stats">
+    <li>
+      <span className="label">Followers</span>
+      <span className="quantity">{stats.followers}</span>
+    </li>
+    <li>
+      <span className="label">Views</span>
+      <span className="quantity">{stats.views}</span>
+    </li>
+    <li>
+      <span className="label">Likes</span>
+      <span className="quantity">{stats.likes}</span>
+    </li>
+  </ul>
+</div>
+
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Profile 
+     username={user.username}
+     tag={user.tag}
+     location={user.location}
+     avatar={user.avatar}
+     stats={user.stats}
+    />
   </React.StrictMode>
 );
