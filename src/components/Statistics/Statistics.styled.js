ffe853c9094786cdc877ae60301c52f11ea37b1e
Darya <daryabobur@gmail.com>
2022-09-09 20:43:35 +0300

    Update Statistics.styled.js

--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const SectionStatistics = styled.section`
 margin-bottom: 50px;
 padding-top: 30px;
+box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.2),
+0px 1px 1px rgba(0, 0, 0, 0.14),
+0px 2px 1px rgba(0, 0, 0, 0.2);
+border-radius: 5px;
 
 .title {
     margin-bottom: 15px;
